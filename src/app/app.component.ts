29fdd9ca9ca20d771ae57bbca17dec65b48e1632
diane-mahoro <www.diammah7@gmail.com>
2019-08-23 15:23:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Quote} from './quote';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  array: Quote[] =[
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+  ];
   title = 'Quote';
 }
