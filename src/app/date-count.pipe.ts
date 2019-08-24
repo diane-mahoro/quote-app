f95ddcf2a13111d3227f51ae707aed63fb804947
diane-mahoro <www.diammah7@gmail.com>
2019-08-24 11:54:46 +0200

    pipes

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,7 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any,): number {
+    let today:Date =new Date();
+    let todaywithnotime:any = new Date(today.getFullYear(), today.getDate())
+    var datedifference = Math.abs(value - todaywithnotime)
+    const secondsInDay = 86400;
+    var datedifferenceseconds = datedifference*0.001;
+    var dateCounter = datedifferenceseconds/secondsInDay;
+    if(dateCounter>=1 && value> todaywithnotime){
+    return dateCounter;
+    }
+    else{
+      return 0;
+    }
     return null;
   }
 
