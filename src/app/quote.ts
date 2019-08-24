9b581413b67e738d3b9a756f35cb8cfe4c391916
diane-mahoro <www.diammah7@gmail.com>
2019-08-24 11:23:27 +0200

    Date and author

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription: boolean;
-    constructor(public id: number,public name: string, public author: string,public postedDate: Date, public description: string){
+    constructor(public id: number,public name: string, public author: string,public dueon: Date, public description: string){
       this.showDescription=false;
     }
 }
